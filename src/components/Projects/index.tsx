6a26b2c656a3ace8041f98d7c33ae703a3c09f8b
xalonious <xander.lauwens@gmail.com>
2024-10-12 16:28:04 +0200

    Added rexx to projects | changed image for portfolio site project

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,6 +3,13 @@ import { motion } from "framer-motion";
 
 const projects = [
     {
+        name: "Rexx",
+        description: "Rexx is a multi-purpose Discord bot that I started working on back in 2020. It was my first major project. The development was discontinued when Discord announced the mandatory switch to slash commands, and I decided not to rewrite the entire codebase.",
+        github: "https://github.com/xalonious/rexx",
+        image: "rexx.png",
+        languages: ["javascript", "nodejs"],
+    },
+    {
         name: "Krestia",
         description: "Krestia is a discord to roblox ranking bot that allows you to remotely manage your roblox group ranks. It provides a seamless integration between Discord and Roblox, making group management easier and more efficient.",
         github: "https://github.com/xalonious/Krestia",
@@ -17,6 +24,13 @@ const projects = [
         languages: ["javascript", "nodejs"],
     },
     {
+        name: "My portfolio website", 
+        description: "The website you're currently on, built with Next.js and Tailwind CSS. It showcases my projects, skills, and experiences, providing a comprehensive overview of my work and capabilities.",
+        github: "https://github.com/xalonious/portfolio",
+        image: "portfolio.png",
+        languages: ["typescript", "react", "nextjs", "css"],
+    },
+    {
         name: "Backup Code Encryptor",
         description: "A simple python program that allows you to encrypt your 2FA backup codes with a password. This tool ensures that your backup codes are securely stored and protected from unauthorized access.",
         github: "https://github.com/xalonious/backup-code-encryptor",
@@ -29,13 +43,6 @@ const projects = [
         github: "https://github.com/xalonious/roblox-in-game-promote-command",
         image: "roblox.png",
         languages: ["javascript", "nodejs", "lua"],
-    },
-    {
-        name: "My portfolio website", 
-        description: "The website you're currently on, built with Next.js and Tailwind CSS. It showcases my projects, skills, and experiences, providing a comprehensive overview of my work and capabilities.",
-        github: "https://github.com/xalonious/portfolio",
-        image: "portfolio.png",
-        languages: ["typescript", "react", "nextjs", "css"],
     },
 ];
 
