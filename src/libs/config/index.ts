fc9e56e1b1b6909904353c6decd00b7c82200c33
xalonious <xander.lauwens@gmail.com>
2025-01-26 21:15:25 +0100

    Changed config title

--- src/libs/config/index.ts
+++ src/libs/config/index.ts
@@ -3,6 +3,6 @@ export const CONFIG = {
 	AKA: "xalonious",
 	NICKNAME: "xander",
 	LOCATION: "Belgium",
-	TITLE: "Backend Developer",
+	TITLE: "Developer",
 	AVATAR_URL: "https://avatars.githubusercontent.com/u/74146476?v=4",
 };
